05a1808e9710f089d878d57b1d11e82ab5663286
Elizeu <loopsdesenvolvedor@gmail.com>
2025-10-24 22:32:01 -0300

    feat: add input para entrada de dados e as funções para conversão de dados.

---
+++ fundamentos/3-input.py
@@ -0,0 +1,12 @@
+# Input -  Entrada de dados.
+# Exmplo.
+
+# O retorno do input e sempre uma string.
+# Converter dados: exemplos abaixo.
+name = input('Digite o nome de filme:\n')
+yearLaunch = int(input('Digite o ano de lançamento: \n'))
+noteMovie = float(input('Digite a nota do filme: \n'))
+
+print(type(name))
+print(type(yearLaunch))
+print(type(noteMovie))
