8cbdd687b09d3abdef1b1653f63f595e377e64bb
Elizeu <loopsdesenvolvedor@gmail.com>
2025-10-25 19:28:35 -0300

    feat: add basico de string

---
+++ fundamentos/6-string.py
@@ -0,0 +1,22 @@
+# String.
+
+movieName = "Top Gun"
+movieName2 = "top Gun"
+
+print(movieName == movieName2) # Case Sensetive.
+
+# String multilinha (aspa duplas ou simples).
+movieDescription = ''' 
+   Top Gun Maverick é um filme de aviação e aventura 
+   muito consagrado na industria.
+'''
+
+# Multiplicação de string.
+line = '='
+
+print(movieName)
+print(line * 20) # Multiplicando.
+print(movieDescription)
+
+# Procurando palavra dentro d eum texto.(Case Sensetive)
+print("Top" in movieName)
