7a9d7acaec4ec90a9faad7c43cd29497493c991a
Elizeu <loopsdesenvolvedor@gmail.com>
2025-10-24 22:20:47 -0300

    feat: tipos de dados e a função type.

---
+++ fundamentos/2-dados.py
@@ -0,0 +1,13 @@
+# Dados em Python.
+# Exemplos.
+name = "Top Gun Maverick"
+yearLaunch = 2023
+noteMovie = 9.5
+planIncluded = False
+
+# Função para saber o tipo de dados = type(dado)
+
+print(type(name)) # retorno: <class 'str'>
+print(type(yearLaunch)) # retorno: <class 'int'>
+print(type(noteMovie)) # retorno: <class 'float'>
+print(type(planIncluded)) # retorno: <class 'bool'>
