8b4d3b5a269632743672b1cb1efe30adf34accae
Elizeu <loopsdesenvolvedor@gmail.com>
2025-10-25 02:28:27 -0300

    feat: operadores em Python.

---
+++ fundamentos/5-opradores.py
@@ -0,0 +1,38 @@
+# Operadores Python.
+
+num1 = int(input("Digite o primeiro número:\n"))
+num2 = int(input("Digite o primeiro número:\n"))
+
+# Aritméticos.
+sum = num1 + num2
+sub = num1 - num2
+div = num1 / num2
+mult = num1 * num2
+mod = num1 % num2
+exp = num1 ** num2
+
+print(sum)
+print(sub)
+print(div)
+print(mult)
+print(mod)
+print(exp)
+
+# Comparação
+bigger = num1 > num2
+smaller = num1 < num2
+equal = num1 == num2
+different = num1 != num2
+biggerEqual = num1 >= num2
+
+print(bigger)
+print(smaller)
+print(equal)
+print(different)
+print(biggerEqual)
+
+#Atribuição
+num1 += 1
+num2 -=1
+num1 *= 1
+num2 /= 1
