b40573e228b4646667c1dac19a891850331bfd7a
Elizeu <loopsdesenvolvedor@gmail.com>
2025-10-28 01:11:31 -0300

    feat: add arrays

---
+++ fundamentos/10-lista.py
@@ -0,0 +1,20 @@
+filmeMatrix = ['Matrix', 199, 8.7, True]
+
+print(filmeMatrix)
+
+
+filmeList = ['Inception', 'The Shawshank Redemption',
+             'The Dark Kgninth', 'Pulp Fiction', 'Interestellar'
+             ]
+
+# Buscando os dois primeiros items da lista.
+print(filmeList[:2])
+
+# Buscando o ultimo item da lista.
+print(filmeList[-1])
+
+# Buscando ate uma determinada posição
+print(filmeList[:3])
+
+# Buscando filme de uma pósição em diante.
+print(filmeList[1:4])
